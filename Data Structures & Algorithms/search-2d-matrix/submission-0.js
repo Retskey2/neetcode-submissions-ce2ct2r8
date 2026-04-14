d88969a43d9cccd6b03c5a9833665a69a77892e5
RETSKEY <71613286+Retskey2@users.noreply.github.com>
2026-04-14 12:08:51 +0300

    Bulk sync: 4 submissions

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.js
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        const nums = matrix.flat()
+
+        let left = 0;
+        let right = nums.length - 1;
+
+        while(left <= right) {
+            const mid = Math.floor((left + right) / 2)
+            const current = nums[mid]
+
+            if(current === target) {
+                return true;
+            }
+
+            if(current < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false
+    }
+}
---
+++ Data Structures & Algorithms/search-2d-matrix/submission-1.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        if (!matrix.length || !matrix[0].length) return false;
+
+        const rows = matrix.length;
+        const cols = matrix[0].length;
+
+        let left = 0;
+        let right = rows * cols - 1
+
+        while(left <= right) {
+            const mid = Math.floor((left + right) / 2);
+           const row = Math.floor(mid / cols);
+           const col = mid % cols;
+
+            const current = matrix[row][col];
+
+            if(current === target) {
+                return true;
+            }
+
+            if(current < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return false
+    }
+}
